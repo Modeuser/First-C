ff1286df3ca329d488fe4458fcf2b6ca0f683c8a
Modeuser <52872086+Modeuser@users.noreply.github.com>
2022-08-15 17:24:40 -0400

    first commit

---
+++ PractiC/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*will be explained later*/
+// it turns out C# comments works here too
+
+/* Printing with variables*/
+//int main()
+//{
+//    char characterName[] = "John";
+//    int characterAge = 35;
+//
+//    printf("There was a man named %s\n", characterName);
+//    printf("he was %d\n", characterAge);
+//
+//    characterAge = 70;
+//    printf("He really liked %s\n", characterName);
+//    printf("but did not like being %d.\n", characterAge);
+//
+//    return 0;
+//}
+
+/* Data types*/
+//int main()
+//{
+//    int age = 40;
+//    //double has double (64) the bits of floats (32)
+//    double gpa = 3.7;
+//    //char can only store one character
+//    char grade = 'F';
+//    //to string a bunch of charaters together, use a char array
+//    char phrase[] = "F for failed";
+//
+//    return 0;
+//}
+
+/* Printf*/
+int main()
+{
+
+
+
+    return 0;
+}
