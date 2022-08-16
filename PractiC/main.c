017d7bd570de6fe98a5a7005f85c76a5c9688133
Modeuser <52872086+Modeuser@users.noreply.github.com>
2022-08-15 20:41:14 -0400

    2 hours into the tutorial

--- PractiC/main.c
+++ PractiC/main.c
@@ -3,7 +3,7 @@
 /*will be explained later*/
 // it turns out C# comments works here too
 
-/* Printing with variables*/
+///* Printing with variables*/
 //int main()
 //{
 //    char characterName[] = "John";
@@ -19,7 +19,7 @@
 //    return 0;
 //}
 
-/* Data types*/
+///* Data types*/
 //int main()
 //{
 //    int age = 40;
@@ -33,11 +33,146 @@
 //    return 0;
 //}
 
-/* Printf*/
+///* Printf*/
+//int main()
+//{
+//    printf("Hello\""); //backslash is a modifier
+//    //%s for string, %d for digits, %c for single characters, %f for floats
+//    printf("My favorite %s is %d", "number", 500); //% is a modifier that acts as a place holder
+//
+//
+//    return 0;
+//}
+
+///* Working with numbers*/
+//int main()
+//{
+//
+//    printf("%f", pow(2, 3));
+//    printf("%f", sqrt(2, 3));
+//    printf("%f", ceil(2, 3)); // always rounds up
+//    printf("%f", floor(2, 3)); // always rounds down
+//
+//    return 0;
+//}
+
+///* Constants*/
+//int main()
+//{
+//    const int num = 5;
+//    printf("%d", num);
+//    num = 8;
+//    printf("%d", num);
+//
+//    return 0;
+//}
+
+///* Getting user inputs*/
+//int main()
+//{
+//    char name[20];
+//    printf("Please enter your name: ");
+//    //scanf("%s", name); //allows the user to input something, and stores it into the "age" variable
+//    //scanf only grabs characters upto the first "space"
+//    fgets(name, 20, stdin); // fgets only works for strings, 20 characters are allowed, "stdin" will ask "fgets" to obtain the input from the terminal
+//    printf("Your name is %s", name);
+//
+//    return 0;
+//}
+
+///* Calculater*/
+//int main()
+//{
+//    double num1;
+//    double num2;
+//    double numf;
+//    int op;
+//
+//    printf("please enter the first number: ");
+//    scanf("%lf", &num1);
+//    printf("please enter the second number: ");
+//    scanf("%lf", &num2);
+//    printf("please enter an operator: ");
+//    scanf("%d", &op);
+//
+//    switch(op) {
+//    case 1:
+//        numf = num1 + num2;
+//        break;
+//    case 2:
+//        numf = num1 - num2;
+//        break;
+//    case 3:
+//        numf = num1 * num2;
+//        break;
+//    default:
+//        printf("operator is not valid");
+//    }
+//
+//    printf("the result is: %lf", numf);
+//    return 0;
+//}
+
+///* Madlib*/
+//int main()
+//{
+//    char color[20];
+//    char pluralNoun[20];
+//    char celebrityF[20];
+//    char celebrityL[20];
+//
+//    printf("Enter a color: ");
+//    scanf("%s", color);
+//    printf("Enter a plural noun: ");
+//    scanf("%s", pluralNoun);
+//    printf("Enter a celebrity: ");
+//    scanf("%s%s", celebrityF, celebrityL);
+//
+//    printf("Roses are %s\n", color);
+//    printf("%s are blue\n", pluralNoun);
+//    printf("I love %s %s\n", celebrityF, celebrityL);
+//
+//    return 0;
+//}
+
+///* Arrays*/
+//int main()
+//{
+//    int luckyNumbers[] = {3, 90, 88, 50, 44};
+//    luckyNumbers[1] = 200;
+//    printf("%d", luckyNumbers[1]);
+//
+//    return 0;
+//}
+
+///* Functions*/
+//int main()
+//{
+//    sayHi();
+//    return 0;
+//}
+//
+//void sayHi (){
+//    printf("hello");
+//}
+
+///* Return functions*/
+// if you want to place this function below "main"
+// you can prototype by placing "double cube(double num)" on the top
+//double cube(double num){
+//    double result = num * num * num;
+//    return result;
+//}
+//
+//int main()
+//{
+//
+//    printf("Answer: %f", cube(3.0));
+//    return 0;
+//}
+
+///* Build a better calculator*/
 int main()
 {
 
-
-
-    return 0;
 }
