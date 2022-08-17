f9e1006b80affb564cdf2672c93529af18c6e0cd
Modeuser <52872086+Modeuser@users.noreply.github.com>
2022-08-17 17:58:45 -0400

    Added a Basic xyxyx calculator

---
+++ Basic xyxyx calculator/main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double num1;
+    double num2;
+    double num3;
+    char cnum1[20];
+    char cnum2[20];
+    char cnum3[20];
+
+    int opID1;
+    int opID2;
+
+    char op1;
+    char op2;
+    char userInput[20];
+
+    double result;
+
+    printf("Enter an operation xyxyx, where x is a number and y is a operator: ");
+    fgets(userInput, 20, stdin);
+    printf("%s", userInput);
+
+    int i;
+    // this "for" statement will find the first operator and id its location
+    for (i = 0; i < 20; i++) {
+        if (userInput[i] == '*' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '/') {
+            op1 = userInput[i];
+            opID1 = i;
+            break;
+        }
+    }
+
+    // this "for" statement will find the second operator and id its location
+    for (i = 20; i > 0; i--) {
+        if (userInput[i] == '*' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '/') {
+            op2 = userInput[i];
+            opID2 = i;
+            break;
+        }
+    }
+
+    // this "for statement will find the first number using opID1's location
+
+    for (i = 0; i < opID1; i++) {
+        cnum1 [i] = userInput[i];
+        num1 = atof(cnum1);
+    }
+    int j = 0;
+    // this "for statement will find the last number using opID2's location
+    for (i = opID2+1; i < 20; i++) {
+        cnum3 [j] = userInput[i];
+        num3 = atof(cnum3);
+        j++;
+    }
+    j = 0;
+    // this "for statement will find the middle number using both ID's location
+    for (i = opID1+1; i < opID2; i++) {
+        cnum2 [j] = userInput[i];
+        num2 = atof(cnum2);
+        j++;
+    }
+
+    //checking for zeros
+    if (num2 == 0 || num3 == 0) {
+        printf("cannot divide by zero");
+        exit(0);
+    }
+
+    //debugging
+    //printf("%c, %c, %s, %d, %d, %d", op1, op2, userInput, num1, num2, num3);
+
+    // these "if" "switch" statements determines which equation to use based on BEDMAS
+    if (op1 == op2) {
+        switch (op1) {
+        case ('+'):
+            result = num1 + num2 + num3;
+            break;
+        case ('-'):
+            result = num1 - num2 - num3;
+            break;
+        case ('*'):
+            result = num1 * num2 * num3;
+            break;
+        case ('/'):
+            result = num1 / num2 / num3;
+            break;
+        }
+    } else if (op1 == '+') {
+        switch (op2) {
+        case ('-'):
+            result = num1 + num2 - num3;
+            break;
+        case ('/'):
+            result = num1 + num2 / num3;
+            break;
+        case ('*'):
+            result = num1 + num2 * num3;
+            break;
+        }
+    } else if (op1 == '-') {
+        switch (op2) {
+        case ('+'):
+            result = num1 - num2 + num3;
+            break;
+        case ('/'):
+            result = num1 - num2 / num3;
+            break;
+        case ('*'):
+            result = num1 - num2 * num3;
+            break;
+        }
+    } else if (op1 == '*') {
+        switch (op2) {
+        case ('-'):
+            result = num1 * num2 - num3;
+            break;
+        case ('+'):
+            result = num1 * num2 + num3;
+            break;
+        case ('/'):
+            result = num1 * num2 / num3;
+            break;
+        }
+    } else if (op1 == '/') {
+        switch (op2) {
+        case ('-'):
+            result = num1 / num2 - num3;
+            break;
+        case ('+'):
+            result = num1 / num2 + num3;
+            break;
+        case ('*'):
+            result = num1 / num2 * num3;
+            break;
+        }
+    }
+    //print the result
+    printf("%f", result);
+
+    return 0;
+}
--- PractiC/main.c
+++ PractiC/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 /*will be explained later*/
 // it turns out C# comments works here too
 
@@ -172,19 +174,21 @@
 //}
 
 ///* Build a better calculator*/
+
 //int main()
 //{
 //    double num1;
 //    double num2;
 //    double numf;
-//    char op;
+//    char op1;
 //
 //    printf("Please enter num1: ");
 //    scanf("%lf", &num1);
+//    printf("Please enter first operator");
+//    scanf(" %c", &op1);
 //    printf("Please enter num2: ");
 //    scanf("%lf", &num2);
-//    printf("Please enter an operator");
-//    scanf(" %c", &op);
+//
 //
 //    switch (op) {
 //    case ('+'):
@@ -195,7 +199,7 @@
 //        break;
 //    case ('/'):
 //        if (num2 == 0) {
-//            printf("cannot divide by 0");
+//            printf(" \ncannot divide by 0");
 //            break;
 //        }
 //        numf = num1 / num2;
@@ -211,6 +215,149 @@
 //
 //    return 0;
 //}
+
+int main()
+{
+    int num1;
+    int num2;
+    int num3;
+    char cnum1[20];
+    char cnum2[20];
+    char cnum3[20];
+
+    int opID1;
+    int opID2;
+
+    char op1;
+    char op2;
+    char userInput[20];
+
+    int result;
+
+
+    printf("Enter an operation xyxyx, where x is a number and y is a operator: ");
+    fgets(userInput, 20, stdin);
+    printf("%s", userInput);
+
+    int i;
+    // this "for" statement will find the first operator and id its location
+    for (i = 0; i < 20; i++) {
+        if (userInput[i] == '*' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '/') {
+            op1 = userInput[i];
+            opID1 = i;
+            break;
+        }
+    }
+
+    // this "for" statement will find the second operator and id its location
+    for (i = 20; i > 0; i--) {
+        if (userInput[i] == '*' || userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '/') {
+            op2 = userInput[i];
+            opID2 = i;
+            break;
+        }
+    }
+
+    // this "for statement will find the first number using opID1's location
+
+    for (i = 0; i < opID1; i++) {
+        cnum1 [i] = userInput[i];
+        num1 = atoi(cnum1);
+    }
+    int j = 0;
+    // this "for statement will find the last number using opID2's location
+    for (i = opID2+1; i < 20; i++) {
+        cnum3 [j] = userInput[i];
+        num3 = atoi(cnum3);
+        j++;
+    }
+    j = 0;
+    // this "for statement will find the middle number using both ID's location
+    for (i = opID1+1; i < opID2; i++) {
+        cnum2 [j] = userInput[i];
+        num2 = atoi(cnum2);
+        j++;
+    }
+
+    //checking for zeros
+    if (num2 == 0 || num3 == 0) {
+        exit(0);
+    }
+
+    //debugging
+    //printf("%c, %c, %s, %d, %d, %d", op1, op2, userInput, num1, num2, num3);
+
+    // these "if" "switch" statements determines which equation to use based on BEDMAS
+    if (op1 == op2) {
+        switch (op1) {
+        case ('+'):
+            result = num1 + num2 + num3;
+            break;
+        case ('-'):
+            result = num1 - num2 - num3;
+            break;
+        case ('*'):
+            result = num1 * num2 * num3;
+            break;
+        case ('/'):
+            result = num1 / num2 / num3;
+            break;
+        }
+    } else if (op1 == '+') {
+        switch (op2) {
+        case ('-'):
+            result = num1 + num2 - num3;
+            break;
+        case ('/'):
+            result = num1 + num2 / num3;
+            break;
+        case ('*'):
+            result = num1 + num2 * num3;
+            break;
+        }
+    } else if (op1 == '-') {
+        switch (op2) {
+        case ('+'):
+            result = num1 - num2 + num3;
+            break;
+        case ('/'):
+            result = num1 - num2 / num3;
+            break;
+        case ('*'):
+            result = num1 - num2 * num3;
+            break;
+        }
+    } else if (op1 == '*') {
+        switch (op2) {
+        case ('-'):
+            result = num1 * num2 - num3;
+            break;
+        case ('+'):
+            result = num1 * num2 + num3;
+            break;
+        case ('/'):
+            result = num1 * num2 / num3;
+            break;
+        }
+    } else if (op1 == '/') {
+        switch (op2) {
+        case ('-'):
+            result = num1 / num2 - num3;
+            break;
+        case ('+'):
+            result = num1 / num2 + num3;
+            break;
+        case ('*'):
+            result = num1 / num2 * num3;
+            break;
+        }
+    }
+    //print the result
+    printf("%d", result);
+
+    return 0;
+}
+
 
 
 ///* Structs*/
@@ -319,15 +466,15 @@
 
 ///* Writing files*/
 
-int main()
-{
-    char line[255];
-
-    FILE * fpointer = fopen("employees.txt", "r"); //replace "w" with "a" to append
-    //fgets (where to store, max size allowed to be read-in, which file?)
-    fgets(line, 255, fpointer);
-    printf("%s", line);
-
-    fclose(fpointer);
-    return 0;
-}
+//int main()
+//{
+//    char line[255];
+//
+//    FILE * fpointer = fopen("employees.txt", "r"); //replace "w" with "a" to append
+//    //fgets (where to store, max size allowed to be read-in, which file?)
+//    fgets(line, 255, fpointer);
+//    printf("%s", line);
+//
+//    fclose(fpointer);
+//    return 0;
+//}
